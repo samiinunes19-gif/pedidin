6f93aed9a46aaf8f0cd35e7edac74c382c1a3c58
samiinunes19 <samiinunes19@github.com>
2026-06-20 13:11:18 -0300

    Fix: unoptimized true para aceitar todos os 44 dominios de imagem

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.multipedidos.com.br' },
-      { protocol: 'https', hostname: 'i.ibb.co' },
-      { protocol: 'https', hostname: 'i.postimg.cc' },
-      { protocol: 'https', hostname: 'down-br.img.susercontent.com' },
-      { protocol: 'https', hostname: 'prod-superon-public-media.s3-sa-east-1.amazonaws.com' }
-    ]
-  },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
